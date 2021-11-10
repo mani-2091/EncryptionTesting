94aba8b98c7459e2af5f4f1f38ee4f4b010e0fab
Joshua Kummer <kummer.joshua@outlook.com>
2021-11-10 10:26:29 +0100

    Projektdateien hinzufügen.

---
+++ EncryptionTesting.Net6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using EncryptionTesting.StandardLib;
+
+namespace EncryptionTesting.Net6
+{
+    internal class Program
+    {
+        private static string PASSPHRASE = "nzv86ri4H2qYHqc&m6rL";
+
+        static void Main(string[] args)
+        {
+            var input = "12345678901234567890";
+            var inputLength = input.Length;
+            var inputBytes = Encoding.UTF8.GetBytes(input);
+
+            var encrypted = StringCipher.Encrypt(input, PASSPHRASE);
+
+            var output = StringCipher.Decrypt(encrypted, PASSPHRASE);
+            var outputLength = output.Length;
+            var outputBytes = Encoding.UTF8.GetBytes(output);
+
+            var lengthDiff = inputLength - outputLength;
+        }
+    }
+}
